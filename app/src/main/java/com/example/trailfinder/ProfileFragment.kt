ebfb1c8559ad1811932623c9a7e72d301fc34679
bens21 <bens21@vt.edu>
2025-10-20 21:52:30 -0400

    Task 1.3 draft

--- app/src/main/java/com/example/trailfinder/ProfileFragment.kt
+++ app/src/main/java/com/example/trailfinder/ProfileFragment.kt
@@ -2,4 +2,11 @@ package com.example.trailfinder
 
 import androidx.fragment.app.Fragment
 
-class ProfileFragment : Fragment(R.layout.fragment_profile)
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.profileUsername).text = "Demo User"
+        view.findViewById<TextView>(R.id.profileAvatar)
+            .setImageResource(R.drawable.ic_person_placeholder)
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,9 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+dependencies {
+    val navVersion = "1.7.0"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+}
